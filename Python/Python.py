e61b20536225da2362e6253b8fe6cbc394ea2606
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-01 23:08:19 +0900

    프로그래머스 : 홀짝에 따라 다른 값 반환하기

--- Python/Python.py
+++ Python/Python.py
@@ -1,7 +1,12 @@
-arr = ['a', 'b', 'c']
-result = ''
-
-for i in range(len(arr)):
-    result += arr[i]
-
-print(result)
+def solution(n):
+    bool = int(n % 2 == 0)
+    sum = 0
+    if(bool) :
+        for i in range(n + 1) :
+            if i % 2 == 0 :
+                sum += i ** 2
+    else :
+        for i in range(n + 1) :
+            if i % 2 == 1 :
+                sum += i
+    return sum
