823b5bf923ff2f8b1fd6398b310a1950b6e09793
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-02 20:26:53 +0900

    프로그래머스 : 코드 처리하기

--- Python/Python.py
+++ Python/Python.py
@@ -1,13 +1,15 @@
-def solution(ineq, eq, n, m):
-    if eq == '=' :
-        if ineq == '>' and n >= m :
-            return 1
-        elif ineq == '<' and n <= m :
-            return 1
-        else : return 0
-    if eq == '!' :
-        if ineq == '>' and n > m :
-            return 1
-        elif ineq == '<' and n < m :
-            return 1
-        else : return 0
+def solution(code):
+    lenght = len(code)
+    mode = 0
+    ret = ''
+    for i in range(lenght) :
+        if code[i] == '1' :
+            mode = int(not(mode))
+            continue
+        if i % 2 == 0 and mode == 0 : 
+            ret += code[i]
+        elif i % 2 == 1 and mode == 1 :
+            ret += code[i]
+    if len(ret) == 0 : 
+        return "EMPTY"
+    else : return ret
