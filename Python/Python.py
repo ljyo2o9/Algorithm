a2ef5a2560d43df73ea692156c5f28d4af2add38
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-02 20:31:29 +0900

    프로그래머스 : 등차수열의 특정한 항만 더하기

--- Python/Python.py
+++ Python/Python.py
@@ -1,15 +1,7 @@
-def solution(code):
-    lenght = len(code)
-    mode = 0
-    ret = ''
-    for i in range(lenght) :
-        if code[i] == '1' :
-            mode = int(not(mode))
-            continue
-        if i % 2 == 0 and mode == 0 : 
-            ret += code[i]
-        elif i % 2 == 1 and mode == 1 :
-            ret += code[i]
-    if len(ret) == 0 : 
-        return "EMPTY"
-    else : return ret
+def solution(a, d, included):
+    sum = 0
+    for i in range(len(included)) :
+        if included[i] :
+            sum += a
+        a += d
+    return sum
