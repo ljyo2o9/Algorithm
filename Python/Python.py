7baec1aa5ffbd9d9f5e9d2ce7a7de3206467c6b1
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-02 21:21:10 +0900

    프로그래머스 : 마지막 두 원소

--- Python/Python.py
+++ Python/Python.py
@@ -1,9 +1,8 @@
 def solution(nlist):
-    even = ''
-    odd = ''
-    for i in range(len(nlist)) :
-        if nlist[i] % 2 == 0 :
-            even = str(even) + str(nlist[i])
-        else :
-            odd = str(odd) + str(nlist[i])
-    return int(even) + int(odd)
+    answer = nlist
+    lenght = len(nlist)
+    if answer[lenght - 1] > answer[lenght - 2] :
+        answer.append(answer[lenght - 1] - answer[lenght - 2])
+    else :
+        answer.append(answer[lenght - 1]*2)
+    return answer
