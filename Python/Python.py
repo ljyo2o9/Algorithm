14fcbefc43ddc29425909364ea31407b491b308a
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-02 20:49:05 +0900

    프로그래머스 : 원소들의 곱과 합

--- Python/Python.py
+++ Python/Python.py
@@ -1,7 +1,10 @@
-def solution(a, b, c):
-    if a != b and a != c and b != c :
-        return (a + b + c)
-    elif (a == b and a != c) or (a == c and a != b) or (b == c and b != a) :
-        return ((a + b + c) * (a**2 + b**2 + c**2))
-    elif a == b and a == c :
-        return ((a + b + c) * (a**2 + b**2 + c**2) * (a**3 + b**3 + c**3))
+def solution(lists):
+    mul = 1
+    sum = 0
+    for i in range(len(lists)) :
+        mul *= lists[i]
+        sum += lists[i]
+    if mul <= (sum**2) :
+        return 1
+    else : 
+        return 0
