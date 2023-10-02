22934eba9cf324b9ea298aa1546d2389b1f2a625
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-02 19:55:52 +0900

    프로그래머스 : 조건 문자열

--- Python/Python.py
+++ Python/Python.py
@@ -1,12 +1,13 @@
-def solution(n):
-    bool = int(n % 2 == 0)
-    sum = 0
-    if(bool) :
-        for i in range(n + 1) :
-            if i % 2 == 0 :
-                sum += i ** 2
-    else :
-        for i in range(n + 1) :
-            if i % 2 == 1 :
-                sum += i
-    return sum
+def solution(ineq, eq, n, m):
+    if eq == '=' :
+        if ineq == '>' and n >= m :
+            return 1
+        elif ineq == '<' and n <= m :
+            return 1
+        else : return 0
+    if eq == '!' :
+        if ineq == '>' and n > m :
+            return 1
+        elif ineq == '<' and n < m :
+            return 1
+        else : return 0
