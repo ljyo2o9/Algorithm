94cfb2b2be6e4796b0c19d0a973d9fa3a8888a48
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-07 22:23:59 +0900

    백준 : 10809번 - 알파벳 찾기

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,19 +1,13 @@
-a = input()
-arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-wndqhr = 0
-max_index = 0
+s = input()
+arr = []
 
-for i in range(len(a)):
-    if ord(a[i]) >= ord('a') :
-        arr[int(ord(a[i]) - ord('a'))] += 1
-    elif ord(a[i]) >= ord("A") :
-        arr[int(ord(a[i]) - ord("A"))] += 1
-    
+for i in range(0, 26):
+    arr.append(-1)
 
-for i in range(len(arr)) :
-    if(arr[i] == max(arr)) :
-        wndqhr += 1
-        max_index = i
+for i in range(len(s)):
+    if arr[int(ord(s[i]) - ord('a'))] != -1 : continue
+    arr[int(ord(s[i]) - ord('a'))] = i
 
-if wndqhr >= 2 : print("?")
-else : print(chr(max_index + ord('A')))
+for i in range(0, 26):
+    print(arr[i], end=' ')
+print('')
