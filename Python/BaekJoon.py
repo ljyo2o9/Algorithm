024235f90873719add26e41dcc5127cf8162403e
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-10 23:23:36 +0900

    백준 : 2609번 - 최대공약수와 최소공배수

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,6 +1,14 @@
-A,B,V = map(int, input().split())
-    
-if (V-B) % (A-B) == 0 :
-    print((V-B)//(A-B))
-else:
-    print((V-B)//(A-B)+1)
+A,B = map(int, input().split())
+
+de = 1
+i = 1
+
+while i <= min(A,B):
+    if A % i == 0 and B % i == 0 :
+        A /= i
+        B /= i
+        de *= i
+        i = 1
+    i += 1
+
+print("%d\n%d" %(de, de * A * B))
