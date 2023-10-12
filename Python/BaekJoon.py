f91e60ffbd386cc10ed0e0a78d08ab7346906265
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-12 09:56:46 +0900

    백준 : 11653번 - 소인수분해

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,12 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-num_list = [0] * 10001
+i = 1
 
-for _ in range(n):
-    num_list[int(sys.stdin.readline())] += 1
-
-for i in range(10001):
-    if num_list[i] != 0:
-        for j in range(num_list[i]):
-            print(i)
+while(i != n) :
+    i += 1
+    if n % i != 0 : continue
+    print(i); n /= i; i = 1
