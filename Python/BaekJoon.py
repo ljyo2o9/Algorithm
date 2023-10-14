926ae14b684265b44f0728b9fc3658a935f0d7d5
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-14 23:30:08 +0900

    백준 : 2309번 - 일곱 난쟁이

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,8 +1,26 @@
 import sys
+from itertools import combinations
 
-n = int(sys.stdin.readline())
-a = int(sys.stdin.readline())
+numbers = []
+sum = 0
 
-print(n + a)
-print(n - a)
-print(n * a)
+def print_arr(combo) :
+    for i in range(len(combo)) :
+        print(combo[i])
+
+for i in range(9) : numbers.append(int(sys.stdin.readline()))
+
+# 7개의 숫자 조합 생성
+combinations_list = list(combinations(numbers, 7))
+# 중복 배열 제거
+unique_combinations = set(tuple(sorted(combo)) for combo in combinations_list)
+
+# 중복을 제외한 조합 출력
+for combo in unique_combinations:
+    for i in range(len(combo)) :
+        sum += combo[i]
+    if sum == 100 : 
+        print_arr(sorted(combo))
+        break
+    sum = 0
+
