90606ec0d717554caf19a280ed169b28f12993c0
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-18 18:17:15 +0900

    백준 : 1037번 - 약수

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,30 +1,9 @@
 import sys
 
 n = int(sys.stdin.readline())
-arr = []
-de = 1
-arr_so = []
+arr = list(map(int, sys.stdin.readline().split()))
 
-def soinsu(a, b) :
-    t = 2
-    while True :
-        if t > a or t > b : break
-        if(a % t == 0 and b % t == 0) :
-            arr.append(t)
-            a //= t; b //= t
-            t = 2
-            continue
-        t += 1
-    arr.append(a); arr.append(b)
+arr.sort()
+m = arr[0] * arr[-1]
 
-for i in range(n) :
-    a, b = map(int, sys.stdin.readline().split())
-    soinsu(a, b)
-    for i in range(len(arr)) :
-        de *= arr[i]
-    arr_so.append(de)
-    de = 1
-    arr.clear()
-
-for i in range(len(arr_so)) :
-    print(int(arr_so[i]))
+print(m)
