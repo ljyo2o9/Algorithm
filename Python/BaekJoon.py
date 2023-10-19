365b49031c1dde47115a73d323f8beca1dc4d399
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-19 21:37:01 +0900

    백준 : 1259번 - 팰린드롬수

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,9 +1,13 @@
 import sys
 
-n = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split()))
+while True :
+    cnt = 0
+    n = int(sys.stdin.readline())
+    if n == 0 : break
 
-arr.sort()
-m = arr[0] * arr[-1]
 
-print(m)
+    for i in range(len(str(n))) :
+        #print(str(n)[i],str(n)[-i - 1])
+        if str(n)[i] != str(n)[-i - 1] : cnt = 1
+    if cnt == 1 : print('no')
+    else : print('yes')
