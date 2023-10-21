b3c37fa0f6345e75102138f663a2219155cb11dc
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-21 13:55:49 +0900

    백준 : 10798번 - 세로읽기

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,13 +1,6 @@
-import sys
+words = [input() for i in range(5)]
 
-while True :
-    cnt = 0
-    n = int(sys.stdin.readline())
-    if n == 0 : break
-
-
-    for i in range(len(str(n))) :
-        #print(str(n)[i],str(n)[-i - 1])
-        if str(n)[i] != str(n)[-i - 1] : cnt = 1
-    if cnt == 1 : print('no')
-    else : print('yes')
+for j in range(15):
+    for i in range(5):
+        if j < len(words[i]):
+            print(words[i][j], end='')
