698256641d81a8d1a3c1c142f3f8b860db7d4e55
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-23 23:18:10 +0900

    백준 : 2752번 - 세수정렬

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,12 +1,10 @@
 import sys
 
-t = int(sys.stdin.readline())
 arr = []
 
-for i in range(t) :
-    cnt = 0
-    n,m = map(str, sys.stdin.readline().split())
-    for i in range(int(n), int(m) + 1) :
-        w = str(i)
-        cnt += w.count('0')
-    print(cnt)
+arr = list(map(int, sys.stdin.readline().split()))
+
+arr.sort()
+
+for i in arr :
+    print(i, end=' ')
