8e4525faff5cfa6209850074cfb9b35fd818fef8
ljyo2o9 <yggosxoljy@gmail.com>
2023-10-27 13:43:56 +0900

    백준 : 2163 - 초콜릿 자르기

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,16 +1,5 @@
 import sys
 
-while True :
-    n = sys.stdin.readline()
-    if int(n) == 0 : break
-    cnt = 0
+n,m = map(int, sys.stdin.readline().split())
 
-    for i in n :
-        if i == '\n' : continue
-        cnt += 1
-        
-        if i == '1' : cnt += 2
-        elif i == '0' : cnt += 4
-        else : cnt += 3
-
-    print(cnt + 1)
+print((n - 1) + (n * (m - 1)))
