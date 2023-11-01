414c292482e6200a633acda60cb147e300a853cd
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-01 09:56:26 +0900

    백준 : 2693번 - N번째 큰 수

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,20 +1,7 @@
-import sys
+n = int(input())
 
-cnt = 0
-arr = []
+for i in range(n) :
+    a = list(map(int, input().split()))
+    a.sort()
 
-while True :
-    n = int(sys.stdin.readline())
-    if n == -1 : break
-
-    for i in range(1, n):
-        if n % i == 0 :
-            cnt += i
-            arr.append(str(i))
-
-    c = ' + '.join(arr)
-    if cnt == n : print(n, "=", c)
-    else : print(n, "is NOT perfect.")
-
-    arr.clear()
-    cnt = 0
+    print(a[-3])
