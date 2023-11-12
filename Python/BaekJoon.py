b72e3dc1d23f532d2eb64aab3c9d301bd380d3e1
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-12 23:36:33 +0900

    백준 : 11005번 - 진법 변환 2

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,32 +1,9 @@
-x, y = map(int, input().split())
-arr = [[0 for _ in range(x)] for _ in range(y)]
+N, B = map(int, input().split())
+s = ''
+arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-for i in range(y) :
-  arr[i] = list(map(int, input().split()))
+while N:
+    s += str(arr[N%B])
+    N //= B
 
-for i in range(y) :
-  arr2 = list(map(int, input().split()))
-
-  arr[i] += arr2
-
-for i in range(y) :
-  for j in range(x) :
-    print(arr[i][j] + arr[i][j + x], end=' ')
-  print()
-
-#   A, B = [], []
-
-# N, M = map(int, input().split())
-
-# for row in range(N):
-#     row = list(map(int, input().split()))
-#     A.append(row)
-
-# for row in range(N):
-#     row = list(map(int, input().split()))
-#     B.append(row)
-    
-# for row in range(N):
-#     for col in range(M):
-#         print(A[row][col] + B[row][col], end=' ')
-#     print()
+print(s[::-1])
