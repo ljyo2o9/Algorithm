1ab456f8e52925f5b49861d7e8c6fab830b4b246
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-13 10:43:34 +0900

    백준 : 2720번 - 세탁소 사장 동혁

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,9 +1,16 @@
-N, B = map(int, input().split())
-s = ''
-arr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#쿼터(Quarter, $0.25)의 개수, 다임(Dime, $0.10)의 개수, 니켈(Nickel, $0.05)의 개수, 페니(Penny, $0.01)
 
-while N:
-    s += str(arr[N%B])
-    N //= B
+n = int(input())
+arr = [0, 0, 0, 0]
 
-print(s[::-1])
+for i in range(n) :
+  k = int(input())
+
+  while k != 0 :
+    if k >= 25 : k -= 25; arr[0] += 1
+    elif k >= 10 : k -= 10; arr[1] += 1
+    elif k >= 5 : k -= 5; arr[2] += 1
+    elif k >= 1 : k -= 1; arr[3] += 1
+
+  print(arr[0], arr[1], arr[2], arr[3])
+  arr = [0, 0, 0, 0]
