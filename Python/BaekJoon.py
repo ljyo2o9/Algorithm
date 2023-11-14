97bf60ee6f705194e189dc6a75092b965366c45e
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-14 12:18:30 +0900

    인프 : 2 ~ n 까지 소수 찾기

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,16 +1,11 @@
-#쿼터(Quarter, $0.25)의 개수, 다임(Dime, $0.10)의 개수, 니켈(Nickel, $0.05)의 개수, 페니(Penny, $0.01)
+#소수 찾기
 
 n = int(input())
-arr = [0, 0, 0, 0]
 
-for i in range(n) :
-  k = int(input())
+sosu = []
+arr = [a for a in range(2, n + 1)]
 
-  while k != 0 :
-    if k >= 25 : k -= 25; arr[0] += 1
-    elif k >= 10 : k -= 10; arr[1] += 1
-    elif k >= 5 : k -= 5; arr[2] += 1
-    elif k >= 1 : k -= 1; arr[3] += 1
-
-  print(arr[0], arr[1], arr[2], arr[3])
-  arr = [0, 0, 0, 0]
+while arr[0] ** 2 <= n :
+  sosu.append(arr[0])
+  arr = [a for a in arr if a % arr[0]]
+print(sosu + arr)
