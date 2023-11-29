ae1e87b7829d409f4ee5b93ff30957e403c45f92
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-29 23:42:31 +0900

    백준 : 10757번 - 큰 수 A + B

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,17 +1,3 @@
-n = int(input())
-m = list(map(int, input().split()))
-cnt = 0
+n,m = input().split(' ')
 
-def sosu(m) :
-  if m == 1 : return 0
-  for i in range(2, m) :
-    if m % i == 0 : return 0
-  return 1
-
-for i in range(n) :
-  p = m[i]
-
-  t = sosu(p)
-  if t == 1 : cnt += 1
-
-print(cnt)
+print(int(n) + int(m))
