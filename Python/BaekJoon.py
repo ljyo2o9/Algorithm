0f4152523eab0cc9c83cf802d56ae8f6df2c33e2
ljyo2o9 <yggosxoljy@gmail.com>
2023-11-30 17:08:58 +0900

    백준 : 1436번 - 영화감독 숌

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,3 +1,13 @@
-n,m = input().split(' ')
+n = int(input())
+cnt = 0; answer = 666; p = []; t = 0
 
-print(int(n) + int(m))
+while True :
+  p = list(str(answer))
+
+  for i in range(len(p) - 2) :
+    if p[i] == '6' and p[i + 1] == '6' and p[i + 2] == '6' : t = 1
+  if t == 1 : cnt += 1
+  if cnt == n : break
+
+  answer += 1; t = 0
+print(answer)
