2e6c3000ac1fba916bf066f18e80cc186c2a9722
ljyo2o9 <yggosxoljy@gmail.com>
2023-12-10 17:33:04 +0900

    백준 : 13706번 - 제곱근

--- Python/BaekJoon.py
+++ Python/BaekJoon.py
@@ -1,13 +1,2 @@
-n = int(input())
-
-words = [str(input()) for i in range(n)]
-
-# 중복 값 제거
-words = list(set(words))
-# 사전 순으로 정렬
-words.sort()
-# 단어 길이 순으로 정렬
-words.sort(key=len)
-
-for i in words:
-    print(i)
+import math
+print(math.isqrt(int(input())))
